5b6bfd5267bda74ed2a3aca9ed57b61eadab5a21
BlueGrave <53641109+BlueGrave@users.noreply.github.com>
2021-04-20 13:05:27 +0800

    Update Sub_Info.js

--- Sub_Info.js
+++ Sub_Info.js
@@ -1,41 +1,63 @@
 /*
-Surge按照注释配置，建议将Name改成机场名字方便区分，作者@mieqq.
+Surge 中显示机场的流量信息
+作者 @mieqq
+
+url请求头中必须带有流量信息, 并且需要urlencode
 
 [Proxy Group]
-Name = select, policy-path=http://t.tt, update-interval=0
+DlerCloud = select, policy-path=http://t.tt?url=, update-interval=3600
 
 [Script]
-Name = type=http-request,pattern=http://t.tt,script-path=sub_info.js
+sub_info = type=http-request,pattern=http://t\.tt,script-path=https://raw.githubusercontent.com/congcong0806/surge-list/master/Script/sub_info.js
 */
 
-const url = "";//引号内填写带有流量信息的订阅
-const due_day = ;//填写重置日期，如1号就填1
+let params = getUrlParams($request.url);
+const url = params.url;
 
-$httpClient.head(url, (err, response) => {
-    let info = getDataInfo(response.headers["subscription-userinfo"]);
-    let days = getRmainingDays();
-    let used = info.download + info.upload;
-    let total = info.total;
-    let body = `${used.toFixed(2)} GB | ${total.toFixed(2)} GB | ${days} Day${days === 1 ? "" : "s"}  = ss, 1.2.3.4, 1234, encrypt-method=aes-128-gcm,password=1234`
-    $done({response:{body}});
-});
+(async () => {
+  let info = await getUserInfo();
+  console.log('info:' + info)
+  let usage = getDataUsage(info);
+  let used = bytesToSize(usage.download + usage.upload);
+  let total = bytesToSize(usage.total);
+  let expire = usage.expire == undefined ? '' : '|' + formatTimestamp(usage.expire * 1000)
+  let body = `${used}/${total}${expire}  = ss, 1.2.3.4, 1234, encrypt-method=aes-128-gcm,password=1234`;
+    $done({response: {body}});
+})();
 
+function getUrlParams(search) {
+    const hashes = search.slice(search.indexOf('?') + 1).split('&')
+    const params = {}
+    hashes.map(hash => {
+        const [key, val] = hash.split('=')
+        params[key] = decodeURIComponent(val)
+    })
+    return params
+}
 
-function getDataInfo(info) {
+function getUserInfo() {
+  return new Promise(resolve => $httpClient.head(url, (err, resp) => resolve(resp.headers["subscription-userinfo"] || resp.headers["Subscription-userinfo"])));
+}
+
+function getDataUsage(info) {
   return Object.fromEntries(
-    info.split("; ").map(item => item.split("="))
-    .map(([k, v]) => [k, (v/1024**3)])
+    info.split("; ").map(item => item.split("=")).map(([k, v]) => [k,parseInt(v)])
   );
 }
 
-function getRmainingDays() {
-  let now = new Date();
-  let today = now.getDate();
-  let month = now.getMonth() + 1;
-  let year = now.getFullYear();
-  let daysInMonth = new Date(year, month, 0).getDate();
-  if (due_day > today) daysInMonth = 0;
-  
-  return daysInMonth - today + due_day;
-  
+function bytesToSize(bytes) {
+    if (bytes === 0) return '0B';
+    var k = 1024;
+    sizes = ['B','KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    i = Math.floor(Math.log(bytes) / Math.log(k));
+    return (bytes / Math.pow(k, i)).toFixed(2) + sizes[i];
+}
+
+function formatTimestamp( timestamp ) {
+    var dateObj = new Date( timestamp );
+    var year = dateObj.getYear() + 1900;
+    var month = dateObj.getMonth() + 1;
+    month = month < 10 ? '0' + month : month
+    var day = dateObj.getDate();
+    return year +"-"+ month +"-" + day;      
 }
