32036dc768c25a69ce2d4dabebfbe3d519f22d03
BlueGrave <BlueGrave@users.noreply.github.com>
2021-12-24 17:13:45 +0800

    Update Covid19.js

--- Scripts/Covid19.js
+++ Scripts/Covid19.js
@@ -1,4 +1,4 @@
-var list = ["杭州","上海","苏州"];
+var list = ["杭州","上海","苏州","绍兴","宁波","西安","内蒙古","黑龙江"];
 const url = "https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5";
 var ala="";
 function nowtime(){
