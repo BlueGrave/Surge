1510298e2dfd306eb031b5f50c35e32e3deb369c
BlueGrave <kanhu.cl@gmail.com>
2026-01-20 13:00:12 +0800

    Update CamScanner.js

--- Scripts/CamScanner.js
+++ Scripts/CamScanner.js
@@ -5,7 +5,7 @@ CamScanner 解锁部分高级特权
 Quantumult X:
 
 [rewrite_local]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/BlueGrave/Surge/master/Scripts/CamScanner.js
 
 [mitm]
 hostname = ap*.intsig.net
@@ -14,21 +14,12 @@ hostname = ap*.intsig.net
 Surge4 or Loon:
 
 [Script]
-http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
-
-[MITM]
-hostname = ap*.intsig.net
-
-***************************
-Quantumult:  
-
-[REWRITE]
-https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
+CamScanner.js = requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/BlueGrave/Surge/master/Scripts/CamScanner.js,type=http-response,pattern=https:\/\/(api|api-cs)-cn\.intsig\.net\/purchase\/cs\/query_property\?,binary-body-mode=0
 
 [MITM]
 hostname = ap*.intsig.net
 
 **************************/
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"3287462400"}}};
+obj = {"data":{"psnl_vip_property":{"expiry":4102415999,"svip": 1,"nxt_renew_tm":4102415999}}};
 $done({body: JSON.stringify(obj)});
